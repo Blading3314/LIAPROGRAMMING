0cd2f4fa82795b4ba43704b79a1813eb378e13dc
Le Tom <thomascastanop@gmail.com>
2025-04-28 19:33:29 -0400

    Initial sharing of project

---
+++ Athletes.java
@@ -0,0 +1,45 @@
+
+/**
+ * Write a description of class Athletes here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Athletes extends Activities
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Athletes
+     */
+    public Athletes()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+   /* public String setGender()
+    {
+        // put your code here
+        return  setGender(); 
+    }
+    
+    public int calculateDistance() { 
+        return
+    } 
+    public int setBurnedCalories(){ 
+        return
+    } 
+    public String getActivity() { 
+        return 
+    } hello
+    */
+   
+}
