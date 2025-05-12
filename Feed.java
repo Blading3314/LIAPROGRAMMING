670c9ea60d4377f9036701b25bbae7dae0861672
Esther-SEA <sohesther.assoa@gmail.com>
2025-05-12 09:37:29 -0400

    better gender

--- Feed.java
+++ Feed.java
@@ -8,8 +8,7 @@ import java.util.List;
  * @version (a version number or a date)
  */
 public class Feed
-{ 
- private List<Athletes> allAthletes = new ArrayList<>();  
+{  
  //private static final List<Athletes> allAthletes = new ArrayList<>();
  public static void main(String[] args) {
     int choice;
@@ -17,11 +16,11 @@ public class Feed
     new Athletes("John", "Doe", Gender.MALE, 1995);
     new Athletes("Jane", "Doe", Gender.FEMALE, 1998);
     
-    new Activities("Walking", TransportMode.WALKING, 5, 20);
-    new Activities("Running", TransportMode.RUNNING, 12, 20);
-    new Activities("Biking", TransportMode.BIKING, 15, 20);
-    new Activities("Swimming", TransportMode.SWIMMING, 18, 20);
-    new Activities("Skating", TransportMode.SKATING, 10, 20);
+    new Activities("Walking", TransportMode.WALKING, 70, 5 );
+    new Activities("Running", TransportMode.RUNNING, 100, 5 );
+    new Activities("Biking", TransportMode.BIKING, 15, 5);
+    new Activities("Swimming", TransportMode.SWIMMING, 18, 1);
+    new Activities("Skating", TransportMode.SKATING, 10, 5);
  
     Scanner mainFeed = new Scanner(System.in);
     System.out.println("Welcome to your new favorite sports app! (๑>؂•̀๑)ᕗ");
@@ -37,56 +36,84 @@ public class Feed
     System.out.println("  (6) Print your details");
     System.out.println("  (7) Calculate burned calories");
     System.out.println();
-    choice = mainFeed.nextInt();
+    //choice = mainFeed.nextInt();
+    do {
+        choice = mainFeed.nextInt();
     
     switch (choice){
-        case 1:
-            System.out.println("Enter your name: ");
-            mainFeed = new Scanner(System.in);
-            String name = mainFeed.nextLine();
-            
-            System.out.println("Enter your last name: ");
-            String lastName = mainFeed.nextLine();
-            
+                case 0: 
+                    System.out.println("Goodbye!");
+                    System.exit(0);
+                    break; 
+                case 1:
+                    System.out.println("Enter your name: ");
+                    mainFeed = new Scanner(System.in);
+                    String name = mainFeed.nextLine();
+                    
+                    System.out.println("Enter your last name: ");
+                    String lastName = mainFeed.nextLine();
+                    
+                    
+        Gender gender = null; 
+        while(gender == null) {
             System.out.println("Enter your gender: (MALE, FEMALE or OTHER)");
-            String genderPut = mainFeed.nextLine().toUpperCase();
-            Gender gender = Gender.valueOf(genderPut);
-            
-            System.out.println("Enter your year of birth: ");
-            int yearOfBirth = mainFeed.nextInt();
-            
-            Athletes athlete = new Athletes(name,lastName,gender,yearOfBirth);
-            System.out.println();
-            System.out.println("Take a look at your profile !");
-            System.out.println(athlete);
-            break;
-            
-        case 2:
-            System.out.println("List of Activities: ");
-            Activities.listActivities();
-            break;
-            
-        case 3:
-          System.out.println("Here is a list of all athletes: ");
-          Athletes.listAllAthletes();
-          break;
+           String genderPut = mainFeed.nextLine().toUpperCase();
         
-        case 4:
-            System.out.println("Here is a list of activities sorted by mode: ");
-            Activities.listActivitiesByTransportMode();
-            break;
-            
-        case 5:
-            System.out.println("Here is a list of activities and their details: ");
-            Activities.listActivitiesDetails();
-            break;
-            
-        case 6:    
-            
-        case 7:
-            System.out.print("Input the activity you've done:");
-          //  Athletes.calculateBurnedCalories();
+        for (Gender g : Gender.values()) {
+            if (g.name().equals(genderPut)) {
+                gender = g;
+                break;
+            }
+        }
+        
+        if (gender == null) {
+            // invalid gender entered
+             System.out.println("Invalid gender entered. Try again");
+        
+           
+        }
     }
-    
+              System.out.println(" You entered: " + gender);
+                System.out.println("Enter your year of birth: ");
+                int yearOfBirth = mainFeed.nextInt();
+                
+                Athletes athlete = new Athletes(name,lastName,gender,yearOfBirth);
+                System.out.println();
+                System.out.println("Take a look at your profile !");
+                System.out.println(athlete);
+                break;
+                
+            case 2:
+                System.out.println("List of Activities: ");
+                Activities.listActivities();
+                break;
+                
+            case 3:
+              System.out.println("Here is a list of all athletes: ");
+              Athletes.listAllAthletes();
+              break;
+            
+            case 4:
+                System.out.println("Here is a list of activities sorted by mode: ");
+                Activities.listActivitiesByTransportMode();
+                break;
+                
+            case 5:
+                System.out.println("Here is a list of activities and their details: ");
+                Activities.listActivitiesDetails();
+                break;
+                
+            case 6:    
+                
+            case 7:
+                System.out.println("Input the activity you've done:");
+              //  Athletes.calculateBurnedCalories();
+              
+              default: 
+                  System.out.println("Not a good choice!");
+                  break;
+        }
+    } while (choice != 7); 
+        
     } 
 }
