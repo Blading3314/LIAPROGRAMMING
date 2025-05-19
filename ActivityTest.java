74a7795bb06b58523ea538414eabe630a870826b
Le Tom <thomascastanop@gmail.com>
2025-05-18 20:56:05 -0400

    added test methods (calories, distance, master list and names

--- ActivityTest.java
+++ ActivityTest.java
@@ -1,42 +1,41 @@
-
-
+// ActivityTest.java
 import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-/**
- * The test class ActivityTest.
- *
- * @author  (your name)
- * @version (a version number or a date)
- */
-public class ActivityTest
-{
-    /**
-     * Default constructor for test class ActivityTest
-     */
-    public ActivityTest()
-    {
-    }
-
-    /**
-     * Sets up the test fixture.
-     *
-     * Called before every test case method.
-     */
+public class ActivityTest {
+    //assertEquals basically checks if the two values are true (thanks w3schools!).
     @BeforeEach
-    public void setUp()
-    {
+    public void setUp() {
+        // clear out any previously created activities so each test is isolated
+        Activity.getAllActivities().clear();
     }
 
-    /**
-     * Tears down the test fixture.
-     *
-     * Called after every test case method.
-     */
-    @AfterEach
-    public void tearDown()
-    {
+    @Test
+    public void testGetCaloriesPerDistance() {
+        // caloriesPerDistance = 5, distance = 10 => expected calories = 50
+        Activity a = new Activity("Late Night Run", TransportMode.RUNNING, 5, 10);
+        assertEquals(50, a.getCaloriesPerDistance(),
+                     "getCaloriesPerDistance should be caloriesPerDistance x distance");
+    }
+
+    @Test
+    public void testCalculateTotalDistanceAll() {
+        // distances: 10 km + 20 km => total = 30 km
+        new Activity("Run",   TransportMode.RUNNING, 1, 10);
+        new Activity("Cycle", TransportMode.CYCLING, 1, 20);
+        assertEquals(30,
+                     Activity.calculateTotalDistanceAll(),
+                     "calculateTotalDistanceAll should be the sum of all activity distances");
+    }
+
+    @Test
+    public void testCalculateTotalCaloriesAll() {
+        // calories: (2 x 5) + (3 x 7) = 10 + 21 = 31
+        new Activity("Jog",    TransportMode.RUNNING, 2, 5);
+        new Activity("Pedal",  TransportMode.CYCLING, 3, 7);
+        assertEquals( (2*5) + (3*7),
+                     Activity.calculateTotalCaloriesAll(),
+                     "calculateTotalCaloriesAll should be the sum of all calories from each activity");
     }
 }
--- AthleteTest.java
+++ AthleteTest.java
@@ -1,42 +1,45 @@
-
-
+// AthleteTest.java
 import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-/**
- * The test class AthleteTest.
- *
- * @author  (your name)
- * @version (a version number or a date)
- */
-public class AthleteTest
-{
-    /**
-     * Default constructor for test class AthleteTest
-     */
-    public AthleteTest()
-    {
-    }
-
-    /**
-     * Sets up the test fixture.
-     *
-     * Called before every test case method.
-     */
+public class AthleteTest {
+    //assertEquals basically checks if the two values are true (thanks w3schools!).
     @BeforeEach
-    public void setUp()
-    {
+    public void setUp() {
+        // ensure no leftover athletes from previous tests
+        Athlete.getAllAthletes().clear();
     }
 
-    /**
-     * Tears down the test fixture.
-     *
-     * Called after every test case method.
-     */
-    @AfterEach
-    public void tearDown()
-    {
+    @Test
+    public void testConstructorAndListAllAthletes() {
+        Athlete ath = new Athlete("Uvuvenvwevuvwen", "Osas", Gender.MALE, 1990);
+       
+        assertEquals(1,
+                     Athlete.getAllAthletes().size(),
+                     "Constructor should add the new athlete to the master list");
+        assertSame(ath,
+                   Athlete.getAllAthletes().get(0),
+                   "The athlete in the list should be exactly the one just created");
+    }
+
+    @Test
+    public void testGetName() {
+        Athlete ath = new Athlete("Jaden", "Smith", Gender.FEMALE, 1992);
+        assertEquals("Jaden Smith", //the name has to be the same as the ath
+                     ath.getName(),
+                     "getName should return firstName + lastName");
+    }
+
+    @Test
+    public void testCalculateBurnedCalories() {
+        Athlete ath = new Athlete("Barack", "Osama", Gender.MALE, 1988);
+        // new    Activity that burns 4 calories/km over 5 km => 20 calories
+        Activity run = new Activity("Trail Run", TransportMode.RUNNING, 4, 5);
+        int burned = ath.calculateBurnedCalories(run);
+         
+        assertEquals(20,
+                     burned,
+                     "calculateBurnedCalories should return the calories for that single activity");
     }
 }
